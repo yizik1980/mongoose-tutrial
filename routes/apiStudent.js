c8941822778d9c79840e5f919068ef311d99a785
Your Name <you@example.com>
2020-12-03 12:09:44 +0200

    adding cors middleware and update validation

--- routes/apiStudent.js
+++ routes/apiStudent.js
@@ -1,14 +1,40 @@
-const express = require('express')
-const student = require('../model/studet');
+const express = require("express");
+const student = require("../model/studet");
+const mongoose = require("mongoose");
 const router = express.Router();
 
-router.post('/new', async (req,res)=>{
-     const s = new student(req.body);
-        const saveRes = await s.save();
-        console.log(saveRes);
-        res.send(req.body);
+router.post("/new", async (req, res) => {
+  const s = new student(req.body);
+  const saveRes = await s.save();
+  console.log(saveRes);
+  res.send(req.body);
 });
-router.put('/update', async (req,res)=>{
-
+router.put("/update/", validateIsStudent, async (req, res) => {
+  req.student = await student.findById(req.body.id);
+  if (!req.student) {
+    res.send("student doesnt exist " + req.student);
+    return;
+  }
+  req.student.name = req.body.name;
+  req.student.last = req.body.last;
+  req.student.age = req.body.age;
+  req.student.gender = req.body.gender;
+  req.student.phone = req.body.phone;
+  const result = await req.student.save();
+  res.send(result);
 });
-module.exports = router
+function validateIsStudent(req, res, next) {
+  if (!req.body.name) {
+    res.send("no student to edit");
+  } else if (!req.body.id) {
+    res.send("student id is require");
+  } else if (
+    !req.body.id.match(/^[0-9a-fA-F]{24}$/) ||
+    !mongoose.Types.ObjectId.isValid(req.body.id)
+  ) {
+    res.send("student id it's not a valid ObjectId");
+  } else {
+    next();
+  }
+}
+module.exports = router;
--- server.js
+++ server.js
@@ -1,28 +1,31 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const dotenv = require('dotenv').config();
-const student = require('./model/studet');
-const studentRoute = require('./routes/apiStudent');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv").config();
+const cors = require("cors");
+const student = require("./model/studet");
+const studentRoute = require("./routes/apiStudent");
 const app = express();
 if (dotenv.error) {
-  throw dotenv.error
+  throw dotenv.error;
 }
-console.log(dotenv.parsed)
+app.use(cors());
 app.use(bodyParser.json());
 
-    mongoose.connect(process.env.MONGO_DB, {
-    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
-    }).catch(err=>{
-        console.log(err);
-    });
-    
+mongoose
+  .connect(process.env.MONGO_DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-app.get('/', async (req, res)=>{
-      
-        const drawStudents = await student.find().sort({ name: 'desc' });
-        res.send(drawStudents);
+app.get("/", async (req, res) => {
+  const drawStudents = await student.find().sort({ name: "desc" });
+  res.send(drawStudents);
 });
-app.use('/student',studentRoute);
+app.use("/student", studentRoute);
 
 app.listen(5000);
