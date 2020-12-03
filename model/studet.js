5ca9db574a37723fdc59a70df5d9e8b3ad4ddab7
Your Name <you@example.com>
2020-12-03 13:12:51 +0200

    adding delete and update functionality

--- model/studet.js
+++ model/studet.js
@@ -1,36 +1,37 @@
-const mongoose = require('mongoose');
-const slugify = require('slugify');
-const dompurify = require('dompurify');
+const mongoose = require("mongoose");
+const slugify = require("slugify");
+const dompurify = require("dompurify");
 
 const studentSechema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
-    },
-    last:{
-        type:String,
-        require:true
-    },
-    age:{
-        type:Number,
-        required:true
-    },
-    gender:{
-        type:String
-    },
-    phone:{
-        type:String
-    }
-    
-
+  name: {
+    type: String,
+    required: true,
+  },
+  last: {
+    type: String,
+    require: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  gender: {
+    type: String,
+    default: "male",
+  },
+  phone: {
+    type: String,
+  },
+  topics: [String],
 });
 
-studentSechema.pre('validate',(next)=>{
-    if(this.name){
-        this.name = dompurify.sanitize(this.name);
-        console.log(name);
-    }
-    next();
+studentSechema.pre("validate", (next) => {
+  console.log(this);
+  if (this.name) {
+    this.name = dompurify.sanitize(this.name);
+    console.log(name);
+  }
+  next();
 });
 
-module.exports = mongoose.model('student', studentSechema);
+module.exports = mongoose.model("student", studentSechema);
--- routes/apiStudent.js
+++ routes/apiStudent.js
@@ -23,6 +23,20 @@ router.put("/update/", validateIsStudent, async (req, res) => {
   const result = await req.student.save();
   res.send(result);
 });
+router.delete("/delete/:id", validateId, async (req, res) => {
+  console.log(req.params);
+  req.student = await student.findByIdAndDelete(req.params.id);
+  if (!req.student) {
+    res.send("student doesnt exist " + req.student);
+    return;
+  }
+  const result = {
+    msg: "success",
+    statusResult: res.statusCode,
+    result: req.student,
+  };
+  res.send(result);
+});
 function validateIsStudent(req, res, next) {
   if (!req.body.name) {
     res.send("no student to edit");
@@ -31,6 +45,18 @@ function validateIsStudent(req, res, next) {
   } else if (
     !req.body.id.match(/^[0-9a-fA-F]{24}$/) ||
     !mongoose.Types.ObjectId.isValid(req.body.id)
+  ) {
+    res.send("student id it's not a valid ObjectId");
+  } else {
+    next();
+  }
+}
+function validateId(req, res, next) {
+  if (!req.params.id) {
+    res.send("student id is require");
+  } else if (
+    !req.params.id.match(/^[0-9a-fA-F]{24}$/) ||
+    !mongoose.Types.ObjectId.isValid(req.params.id)
   ) {
     res.send("student id it's not a valid ObjectId");
   } else {
